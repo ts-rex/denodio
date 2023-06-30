699494d5171fe187cf5dabaab12378dc2c098142
maverickquill55 <consorceo300@icloud.com>
2023-06-30 11:53:12 -0400

    Allow ArrayBufferLike for buffer option

--- mod.ts
+++ mod.ts
@@ -4,7 +4,7 @@ export type Options = {
 	/**
 	 * Uint8Array
 	 */
-	buffer: Uint8Array
+	buffer: Uint8Array | ArrayBufferLike
 	volume?: number
 	speed?: number
 	use_spatial: boolean
@@ -31,7 +31,7 @@ export async function play(options: Options) {
     // @ts-ignore: Generated types don't always match what the library actually needs.
 	const newOptions: internal.Options = {
 		...options,
-		buffer: Array.from(options.buffer),
+		buffer: Array.from(new Uint8Array(options.buffer)),
 	}
     if(newOptions.speed && (newOptions.speed > 1 || newOptions.speed < 0)) {
         throw new Error('speed must be between 0 and 1')
